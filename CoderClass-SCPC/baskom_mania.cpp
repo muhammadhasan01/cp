00a9cd4b057f188ec01c399e2a3e6759b34736cb
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2019-07-19 02:02:14 +0700

    initial commit

---
+++ CoderClass-SCPC/baskom_mania.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5+5;
+int tc;
+int n;
+long long m,k;
+long long a[N], pre[N];
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> tc;
+    while(tc--){
+        cin >> n >> m >> k;
+        for(int i=1;i<=n;i++){
+            cin >> a[i];
+        }
+    }
+
+
+    return 0;
+}

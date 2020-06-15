4dde636a3e311ad58314ddbe3d5952a618e91302
Muhammad Hasan <muhammadhasan50@gmail.com>
2020-06-16 05:24:46 +0700

    Edit Djikstra on Sparse Graph

--- Algorithms/Djikstra.cpp
+++ Algorithms/Djikstra.cpp
@@ -1,4 +1,68 @@
-// https://cp-algorithms.com/graph/dijkstra.html
+/**
+https://cp-algorithms.com/graph/dijkstra_sparse.html
+**/
+
+const int INF = 1000000000;
+vector<vector<pair<int, int>>> adj;
+
+void dijkstra(int s, vector<int> & d, vector<int> & p) {
+    int n = adj.size();
+    d.assign(n, INF);
+    p.assign(n, -1);
+
+    d[s] = 0;
+    set<pair<int, int>> q;
+    q.insert({0, s});
+    while (!q.empty()) {
+        int v = q.begin()->second;
+        q.erase(q.begin());
+
+        for (auto edge : adj[v]) {
+            int to = edge.first;
+            int len = edge.second;
+
+            if (d[v] + len < d[to]) {
+                q.erase({d[to], to});
+                d[to] = d[v] + len;
+                p[to] = v;
+                q.insert({d[to], to});
+            }
+        }
+    }
+}
+
+void dijkstra(int s, vector<int> & d, vector<int> & p) {
+    int n = adj.size();
+    d.assign(n, INF);
+    p.assign(n, -1);
+
+    d[s] = 0;
+    using pii = pair<int, int>;
+    priority_queue<pii, vector<pii>, greater<pii>> q;
+    q.push({0, s});
+    while (!q.empty()) {
+        int v = q.top().second;
+        int d_v = q.top().first;
+        q.pop();
+        if (d_v != d[v])
+            continue;
+
+        for (auto edge : adj[v]) {
+            int to = edge.first;
+            int len = edge.second;
+
+            if (d[v] + len < d[to]) {
+                d[to] = d[v] + len;
+                p[to] = v;
+                q.push({d[to], to});
+            }
+        }
+    }
+}
+
+/**
+https://cp-algorithms.com/graph/dijkstra.html
+**/
 
 const int INF = 1000000000;
 vector<vector<pair<int, int>>> adj;

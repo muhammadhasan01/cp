4ba9712dca00e5c9e279461311230b22ebc25760
Muhammad Hasan <muhammadhasan50@gmail.com>
2020-07-02 16:19:05 +0700

    Fixed name contest + upsolve

--- Online Judge/Codeforces/Contests/DIV2/DIV2-654/E.cpp
+++ Online Judge/Codeforces/Contests/DIV2/DIV2-654/E.cpp
@@ -2,11 +2,16 @@
 
 using namespace std;
 
-const int N = 2e3 + 5;
+const int N = 1e5 + 5;
+const int INF = 1e9 + 100;
 
 int n, p;
 int a[N];
-vector<int> answer;
+
+bool can(int x, int i) {
+    int len = min(n, n - (a[i] - x)) - (n - i);
+    return (len < p);
+}
 
 int main() { 
     ios_base::sync_with_stdio(0);
@@ -18,12 +23,28 @@ int main() {
         cin >> a[i];
     }
     sort(a + 1, a + 1 + n);
+    int left_bound = 0;
     for (int i = 1; i <= n; i++) {
-        
+        left_bound = max(left_bound, a[i] - i + 1);
     }
-    cout << answer.size() << '\n';
-    for (auto& e : answer) cout << e << " ";
-    cout << '\n';
+    int right_bound = INF;
+    for (int i = p; i <= n; i++) {
+        int l = left_bound, r = INF, at = left_bound - 1;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if (can(mid, i)) {
+                l = mid + 1;
+                at = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+        right_bound = min(right_bound, at);
+    }
+    cout << right_bound - left_bound + 1 << '\n';
+    for (int x = left_bound; x <= right_bound; x++) {
+        cout << x << (x == right_bound ? '\n' : ' ');
+    }
 
     return 0;
 }
--- Online Judge/Codeforces/Contests/DIV3/DIV3-653/E.cpp
+++ Online Judge/Codeforces/Contests/DIV3/DIV3-653/E.cpp
@@ -2,85 +2,57 @@
 
 using namespace std;
 
-const int N = 2e5 + 5;
-
-struct book {
-    int t, a, b;
-};
-
-int n, k;
-book p[N];
-vector<int> v[3];
-
-void dismiss() {
-    cout << -1 << '\n';
-    exit(0);
-}
-
 int main() { 
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
+    int n, k;
     cin >> n >> k;
-    int cnt_a = 0, cnt_b = 0;
-    int res = 0;
-    for (int i = 1; i <= n; i++) {
-        cin >> p[i].t >> p[i].a >> p[i].b;
-        if (p[i].a && p[i].b) {
-            cnt_a++, cnt_b++;
-            v[2].emplace_back(p[i].t);
-        } else if (p[i].a) {
-            cnt_a++;
-            v[0].emplace_back(p[i].t);
-        } else if (p[i].b) {
-            cnt_b++;
-            v[1].emplace_back(p[i].t);
-        } else {
-            continue;
-        }
-        res += p[i].t;
+    vector<int> v[4];
+    vector<int> len(4);
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        int t, a, b;
+        cin >> t >> a >> b;
+        int cur = ((a & 1) << 1) + (b & 1);
+        if (cur == 0) continue;
+        sum += t;
+        v[cur].emplace_back(t);
+        len[cur]++;
     }
-    if (cnt_a < k || cnt_b < k) dismiss();
-    for (int i = 0; i < 3; i++) sort(v[i].begin(), v[i].end());
-    while (cnt_a > k && cnt_b > k) {
-        if (!v[0].empty() && !v[1].empty()) {
-            int cur = v[0].back() + v[1].back();
-            int cur_2 = (v[2].empty() ? -1 : v[2].back());
-            if (cur_2 >= cur) {
-                res -= cur_2;
-                v[2].pop_back();
-            } else {
-                if (v[0].back() >= v[1].back()) {
-                    res -= v[0].back();
-                    v[0].pop_back();
-                    cnt_a--;
-                } else {
-                    res -= v[1].back();
-                    v[1].pop_back();
-                    cnt_b--;
-                }
+    if (len[1] + len[3] < k || len[2] + len[3] < k) {
+        puts("-1");
+        return 0;
+    }
+    vector<int> pre[4];
+    for (int i = 1; i <= 3; i++) sort(v[i].begin(), v[i].end(), greater<int>());
+    for (int i = 1; i <= 2; i++) {
+        pre[i].resize(len[i]);
+        for (int j = 0; j < len[i]; j++) {
+            if (j) pre[i][j] = pre[i][j - 1];
+            pre[i][j] += v[i][j];
+        }
+    }
+    int ans = 2e9;
+    int val = 0;
+    for (int sz = 0; sz <= len[3]; sz++) {
+        if (sz) val += v[3][sz - 1];
+        int curval = val;
+        bool ok = true;
+        for (int j = 1; j <= 2; j++) {
+            int cursz = len[j] + len[3] - sz - k;
+            if (cursz > len[j] || cursz < 0) {
+                ok = false;
+                break;
             }
-        } else {
-            if (v[2].empty()) break;
-            res -= v[2].back();
-            v[2].pop_back();
+            if (cursz) curval += pre[j][cursz - 1];
         }
-        cnt_a--, cnt_b--;
+        if (!ok) continue;
+        curval = sum - curval;
+        ans = min(ans, curval);
     }
-    while (cnt_a > k) {
-        if (v[0].empty()) break;
-        res -= v[0].back();
-        v[0].pop_back();
-        cnt_a--;
-    }
-    while (cnt_b > k) {
-        if (v[1].empty()) break;
-        res -= v[1].back();
-        v[1].pop_back();
-        cnt_b--;
-    }
-    cout << res << '\n';
+    cout << ans << '\n';
 
     return 0;
 }

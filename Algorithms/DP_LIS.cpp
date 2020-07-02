6f95d5f8dcd62e69c910c11ba49655586e715300
Muhammad Hasan <muhammadhasan50@gmail.com>
2020-07-02 19:26:35 +0700

    Add LIS from cp-algorithms

--- Algorithms/DP_LIS.cpp
+++ Algorithms/DP_LIS.cpp
@@ -48,6 +48,26 @@ int iterative_dp(){
 }
 
 // O(N Log N) solution
+int lis(vector<int> const& a) {
+    int n = a.size();
+    const int INF = 1e9;
+    vector<int> d(n+1, INF);
+    d[0] = -INF;
+
+    for (int i = 0; i < n; i++) {
+        int j = upper_bound(d.begin(), d.end(), a[i]) - d.begin();
+        if (d[j-1] < a[i] && a[i] < d[j])
+            d[j] = a[i];
+    }
+
+    int ans = 0;
+    for (int i = 0; i <= n; i++) {
+        if (d[i] < INF)
+            ans = i;
+    }
+    return ans;
+}
+
 int lis(){
     int tail[n+3], len = 1;
     MEM(tail,0);

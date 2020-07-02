5e5381c38c5953ba61481750a252731de7a37298
Muhammad Hasan <muhammadhasan50@gmail.com>
2020-07-03 01:57:21 +0700

    Reorganize

--- Online Judge/Codeforces/Practice/Suffix_Array_EDU/Longest_Common_Substring.cpp
+++ Online Judge/Codeforces/Practice/Suffix_Array_EDU/Longest_Common_Substring.cpp
@@ -1,22 +1,21 @@
-// https://cp-algorithms.com/string/suffix-array.html
 #include <bits/stdc++.h>
 
 using namespace std;
 
-vector<int> sort_cyclic_shifts(string const& s) {
+vector<int> sort_cyclic_shifts(const string& s) {
     int n = s.size();
     const int alphabet = 256;
     vector<int> p(n), c(n), cnt(max(alphabet, n), 0);
     for (int i = 0; i < n; i++)
         cnt[s[i]]++;
     for (int i = 1; i < alphabet; i++)
-        cnt[i] += cnt[i-1];
+        cnt[i] += cnt[i - 1];
     for (int i = 0; i < n; i++)
         p[--cnt[s[i]]] = i;
     c[p[0]] = 0;
     int classes = 1;
     for (int i = 1; i < n; i++) {
-        if (s[p[i]] != s[p[i-1]])
+        if (s[p[i]] != s[p[i - 1]])
             classes++;
         c[p[i]] = classes - 1;
     }
@@ -31,14 +30,14 @@ vector<int> sort_cyclic_shifts(string const& s) {
         for (int i = 0; i < n; i++)
             cnt[c[pn[i]]]++;
         for (int i = 1; i < classes; i++)
-            cnt[i] += cnt[i-1];
-        for (int i = n-1; i >= 0; i--)
+            cnt[i] += cnt[i - 1];
+        for (int i = n - 1; i >= 0; i--)
             p[--cnt[c[pn[i]]]] = pn[i];
         cn[p[0]] = 0;
         classes = 1;
         for (int i = 1; i < n; i++) {
             pair<int, int> cur = {c[p[i]], c[(p[i] + (1 << h)) % n]};
-            pair<int, int> prev = {c[p[i-1]], c[(p[i-1] + (1 << h)) % n]};
+            pair<int, int> prev = {c[p[i - 1]], c[(p[i - 1] + (1 << h)) % n]};
             if (cur != prev)
                 ++classes;
             cn[p[i]] = classes - 1;
@@ -48,29 +47,9 @@ vector<int> sort_cyclic_shifts(string const& s) {
     return p;
 }
 
-int compare(int i, int j, int l, int k) {
-    pair<int, int> a = {c[k][i], c[k][(i+l-(1 << k))%n]};
-    pair<int, int> b = {c[k][j], c[k][(j+l-(1 << k))%n]};
-    return a == b ? 0 : a < b ? -1 : 1;
-}
-
-vector<int> suffix_array_construction(string s) {
-    s += "$";
+vector<int> suffix_array_construction(string const& s) {
     vector<int> sorted_shifts = sort_cyclic_shifts(s);
-    sorted_shifts.erase(sorted_shifts.begin());
     return sorted_shifts;
-}
-
-int lcp(int i, int j) {
-    int ans = 0;
-    for (int k = log_n; k >= 0; k--) {
-        if (c[k][i] == c[k][j]) {
-            ans += 1 << k;
-            i += 1 << k;
-            j += 1 << k;
-        }
-    }
-    return ans;
 }
 
 vector<int> lcp_construction(string const& s, vector<int> const& p) {
@@ -96,4 +75,43 @@ vector<int> lcp_construction(string const& s, vector<int> const& p) {
     return lcp;
 }
 
-// number of different substring = n * (n + 1) / 2 - sum_(i = 0 to n - 2) lcp[i]
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s, t;
+    cin >> s >> t;
+    int n = s.length();
+    int m = t.length();
+    s = s + "$" + t + "#";
+    vector<int> p = suffix_array_construction(s);
+    vector<int> lcp = lcp_construction(s, p);
+    int len = p.size();
+    vector<int> place(len);
+    for (int i = 0; i < len; i++) {
+        if (p[i] < n) {
+            place[i] = 1;
+        } else if (n + m >= p[i] && p[i] > n) {
+            place[i] = 2;
+        } else {
+            place[i] = 3;
+        }
+    }
+    string answer = "";
+    int ans = 0;
+    for (int i = 0; i < len - 1; i++) {
+        if (place[i] == 3 || place[i + 1] == 3) continue;
+        if (place[i] == place[i + 1]) continue;
+        if (lcp[i] > ans) {
+            answer = s.substr(p[i], lcp[i]);
+            ans = lcp[i];
+        } else if (lcp[i] == ans) {
+            string cur = s.substr(p[i], lcp[i]);
+            if (cur < answer) answer = cur;
+        }
+    }
+    cout << answer << '\n';
+
+    return 0;
+}
--- Online Judge/Codeforces/Practice/Suffix_Array_EDU/Number_Of_Different_Substrings.cpp
+++ Online Judge/Codeforces/Practice/Suffix_Array_EDU/Number_Of_Different_Substrings.cpp
@@ -1,22 +1,21 @@
-// https://cp-algorithms.com/string/suffix-array.html
 #include <bits/stdc++.h>
 
 using namespace std;
 
-vector<int> sort_cyclic_shifts(string const& s) {
+vector<int> sort_cyclic_shifts(const string& s) {
     int n = s.size();
     const int alphabet = 256;
     vector<int> p(n), c(n), cnt(max(alphabet, n), 0);
     for (int i = 0; i < n; i++)
         cnt[s[i]]++;
     for (int i = 1; i < alphabet; i++)
-        cnt[i] += cnt[i-1];
+        cnt[i] += cnt[i - 1];
     for (int i = 0; i < n; i++)
         p[--cnt[s[i]]] = i;
     c[p[0]] = 0;
     int classes = 1;
     for (int i = 1; i < n; i++) {
-        if (s[p[i]] != s[p[i-1]])
+        if (s[p[i]] != s[p[i - 1]])
             classes++;
         c[p[i]] = classes - 1;
     }
@@ -31,14 +30,14 @@ vector<int> sort_cyclic_shifts(string const& s) {
         for (int i = 0; i < n; i++)
             cnt[c[pn[i]]]++;
         for (int i = 1; i < classes; i++)
-            cnt[i] += cnt[i-1];
-        for (int i = n-1; i >= 0; i--)
+            cnt[i] += cnt[i - 1];
+        for (int i = n - 1; i >= 0; i--)
             p[--cnt[c[pn[i]]]] = pn[i];
         cn[p[0]] = 0;
         classes = 1;
         for (int i = 1; i < n; i++) {
             pair<int, int> cur = {c[p[i]], c[(p[i] + (1 << h)) % n]};
-            pair<int, int> prev = {c[p[i-1]], c[(p[i-1] + (1 << h)) % n]};
+            pair<int, int> prev = {c[p[i - 1]], c[(p[i - 1] + (1 << h)) % n]};
             if (cur != prev)
                 ++classes;
             cn[p[i]] = classes - 1;
@@ -48,29 +47,11 @@ vector<int> sort_cyclic_shifts(string const& s) {
     return p;
 }
 
-int compare(int i, int j, int l, int k) {
-    pair<int, int> a = {c[k][i], c[k][(i+l-(1 << k))%n]};
-    pair<int, int> b = {c[k][j], c[k][(j+l-(1 << k))%n]};
-    return a == b ? 0 : a < b ? -1 : 1;
-}
-
 vector<int> suffix_array_construction(string s) {
     s += "$";
     vector<int> sorted_shifts = sort_cyclic_shifts(s);
     sorted_shifts.erase(sorted_shifts.begin());
     return sorted_shifts;
-}
-
-int lcp(int i, int j) {
-    int ans = 0;
-    for (int k = log_n; k >= 0; k--) {
-        if (c[k][i] == c[k][j]) {
-            ans += 1 << k;
-            i += 1 << k;
-            j += 1 << k;
-        }
-    }
-    return ans;
 }
 
 vector<int> lcp_construction(string const& s, vector<int> const& p) {
@@ -96,4 +77,21 @@ vector<int> lcp_construction(string const& s, vector<int> const& p) {
     return lcp;
 }
 
-// number of different substring = n * (n + 1) / 2 - sum_(i = 0 to n - 2) lcp[i]
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s;
+    cin >> s;
+    vector<int> p = suffix_array_construction(s);
+    vector<int> lcp = lcp_construction(s, p);
+    int n = s.length();
+    long long res = (1LL) * n * (n + 1) / 2;
+    for (auto& e : lcp) {
+        res -= (1LL) * e;
+    }
+    cout << res << '\n';
+
+    return 0;
+}

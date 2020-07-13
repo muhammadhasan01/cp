3534f3c83f1f414e47083b7445a15d43498fe35c
Muhammad Hasan <muhammadhasan50@gmail.com>
2020-07-13 20:43:57 +0700

    Fastscan

---
+++ Algorithms/Fastscan.cpp
@@ -0,0 +1,10 @@
+void fastscan(int &x)
+{
+    register int c = getchar();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = getchar());
+    if(c=='-') {neg=1;c=getchar();}
+    for(;c>47 && c<58;c = getchar()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}

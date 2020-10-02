1f84293d2847e2c10b5fdfbad92de8bad377cc42
Muhammad Hasan <muhammadhasan50@gmail.com>
2020-10-02 08:21:14 +0700

    Add powerup spoj

---
+++ Online Judge/SPOJ/POWERUP.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/** START OF TEMPLATE DEBUGGER **/
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+/** END OF TEMPLATE DEBUGGER **/
+
+const long long M = 1e9 + 7;
+
+long long modpow(long long x, long long y, long long p) {
+    long long ret = 1;
+    while (y) {
+        if (y & 1) ret = (ret * x) % p;
+        y >>= 1;
+        x = (x * x) % p;
+    }
+    return ret;
+}
+
+void solve() {
+    long long a, b, c;
+    cin >> a >> b >> c;
+    if (a == b && b == c && c == -1) exit(0);
+    long long ret = -1;
+    if (c == 0)
+        ret = a % M;
+    else if (b == 0)
+        ret = 1;
+    else if (a % M == 0)
+        ret = 0;
+    else {
+        ret = modpow(b, c, M - 1);
+        ret = modpow(a, ret, M);
+    }
+    cout << ret << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    while (1) {
+        solve();
+    }
+
+    return 0;
+}

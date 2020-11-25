3c86869104eab87c267346932c8dc4683d241d62
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2020-11-25 07:10:01 +0700

    Add DIV3-686D

---
+++ Online Judge/Codeforces/Contests/DIV3/DIV3-686/D.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+void solve() {
+    long long n;
+    cin >> n;
+    long long bef = n;
+    vector<pair<long long, int>> v;
+    for (long long i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            int cnt = 0;
+            while (n % i == 0) {
+                n /= i;
+                ++cnt;
+            }
+            v.emplace_back(i, cnt);
+        }
+    }
+    if (n > 1) v.emplace_back(n, 1);
+    sort(v.begin(), v.end(), [&](auto& x, auto& y) {
+        return x.second > y.second;
+    });
+    int k = v[0].second;
+    cout << k << '\n';
+    for (int i = 0; i < k - 1; i++) {
+        cout << v[0].first << ' ';
+        bef /= v[0].first;
+    }
+    cout << bef << '\n';
+}
+
+int main() { 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc;
+    cin >> tc;
+    for (int T = 1; T <= tc; T++) {
+        solve();
+    }
+
+    return 0;
+}

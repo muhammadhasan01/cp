8bf9b8a15c4965a1847927ffba276fcadd86c4bc
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2020-12-03 17:21:15 +0700

    Add nCr to math library

---
+++ Library/Math/nCr.cpp
@@ -0,0 +1,35 @@
+
+const int M = 1e9 + 7;
+const int N = 2e5 + 5;
+
+long long fact[N];
+long long invf[N];
+
+long long modpow(long long x, long long y) {
+    long long ret = 1;
+    while (y > 0) {
+        if (y & 1) ret = (ret * x) % M;
+        y >>= 1;
+        x = (x * x) % M;
+    }
+    return ret;
+}
+
+long long inv(long long x) {
+    return modpow(x, M - 2);
+}
+
+long long C(int a, int b) {
+    if (a < b) return 0;
+    long long ret = (fact[a] * invf[b]) % M;
+    ret = (ret * invf[a - b]) % M;
+    return ret;
+}
+
+void init() {
+    fact[0] = invf[0] = 1;
+    for (int i = 1; i < N; i++) {
+        fact[i] = (fact[i - 1] * i) % M;
+        invf[i] = inv(fact[i]);
+    }
+}

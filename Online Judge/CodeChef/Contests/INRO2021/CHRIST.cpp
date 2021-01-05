f0c84f15db94bd768f3f41cb7f1e73940ed11d90
Muhammad Hasan <muhammadhasan50@gmail.com>
2021-01-06 04:42:07 +0700

    Add CHRIST codechef

---
+++ Online Judge/CodeChef/Contests/INRO2021/CHRIST.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+const int N = 2e7 + 5;
+
+vector<int> vals;
+bool not_prime[N];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    vals.emplace_back(1);
+    for (int i = 2; i < N; i++) {
+        if (not_prime[i]) continue;
+        vals.emplace_back(i);
+        for (int j = i + i; j < N; j += i) {
+            not_prime[j] = 1;
+        }
+    }
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        long long res = (1LL) * vals[i] * n;
+        cout << res << (i == n - 1 ? '\n' : ' ');
+    }
+
+    return 0;
+}

2e22cea0a0bda4aca571f186f973ff09c5ef9181
Muhammad Hasan <muhammadhasan50@gmail.com>
2021-01-10 06:56:51 +0700

    I is EZ

---
+++ Online Judge/Codeforces/Gym/2017-2018 ACM-ICPC German Collegiate Programming Contest (GCPC 2017)/I.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    vector<int> x(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> x[i];
+    }
+    vector<int> dp(n + 1);
+    vector<int> pre(n + 1);
+    for (int i = 1; i <= n; i++) {
+        if (i > m)
+            dp[i] = x[i] + pre[i - m];
+        pre[i] = max(dp[i], pre[i - 1]);
+    }
+    cout << *max_element(dp.begin(), dp.end()) << '\n';
+
+    return 0;
+}

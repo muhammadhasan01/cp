cd353d250cd73bc665c92d74fe6a38f37d720ca0
Muhammad Hasan <muhammadhasan50@gmail.com>
2021-01-12 07:58:19 +0700

    Ayo Div3

---
+++ Online Judge/Codeforces/Contests/DIV3/DIV3-693/A.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+void solve() {
+    long long w, h, n;
+    cin >> w >> h >> n;
+
+    auto get = [&](long long x) -> long long {
+        long long ret = 1;
+        while (x % 2 == 0) {
+            x /= 2;
+            ret = ret + ret;
+        }
+        return ret;
+    };
+
+    long long res = get(w) * get(h);
+    cout << (res >= n ? "YES" : "NO") << '\n';
+}
+
+int main() { 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc;
+    cin >> tc;
+    for (int T = 1; T <= tc; T++) {
+        solve();
+    }
+
+    return 0;
+}

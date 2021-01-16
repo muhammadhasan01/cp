059f547e1d0130ba9210613c9ef9f9e8e4a08ec5
Muhammad Hasan <muhammadhasan50@gmail.com>
2021-01-16 09:15:22 +0700

    add B DIV3

---
+++ Online Judge/Codeforces/Contests/DIV3/DIV3-693/B.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+const int K = 300;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n + 1);
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        sum += a[i];
+    }
+    if (sum & 1) {
+        cout << "NO" << '\n';
+        return;
+    }
+    vector<vector<bool>> dp(n + 1, vector<bool>(K));
+    dp[0][0] = true;
+    for (int i = 1; i <= n; i++) {
+        for (int val = 0; val < K; val++) {
+            if (dp[i - 1][val]) {
+                dp[i][val] = true;
+            }
+            if (val - a[i] >= 0 && dp[i - 1][val - a[i]]) {
+                dp[i][val] = true;
+            }
+        }
+    }
+    cout << (dp[n][sum / 2] ? "YES" : "NO") << '\n';
+}
+
+int main() { 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc;
+    cin >> tc;
+    for (int T = 1; T <= tc; T++) {
+        solve();
+    }
+
+    return 0;
+}

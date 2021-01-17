41aca7fe0e936ad73635c9c4eefc31c7150b31a2
Muhammad Hasan <muhammadhasan50@gmail.com>
2021-01-17 10:58:15 +0700

    Katryoshka

---
+++ Online Judge/Codeforces/Gym/Egyptian Collegiate Programming Contest 2017 (ACM ECPC 2017)/K.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+void solve() {
+    long long n, m, k;
+    cin >> n >> m >> k;
+    long long z = min(n, min(m, k));
+    n -= z; m -= z; k -= z;
+    cout << z + min(n / 2, k) << '\n';
+}
+
+int main() { 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    freopen("katryoshka.in", "r", stdin); 
+
+    int tc;
+    cin >> tc;
+    for (int T = 1; T <= tc; T++) {
+        cout << "Case " << T << ": ";
+        solve();
+    }
+
+    return 0;
+}

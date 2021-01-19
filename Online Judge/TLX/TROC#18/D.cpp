8adf8c83de0bca92096db015852ba8db8251ddde
Muhammad Hasan <muhammadhasan50@gmail.com>
2021-01-20 06:54:49 +0700

    D TROC

---
+++ Online Judge/TLX/TROC#18/D.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+const int N = 1e2 + 5;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    vector<int> dp(N, 3);
+    dp[1] = 3; dp[2] = 4;
+    dp[3] = 4; dp[4] = 3;
+    dp[5] = 4; dp[6] = 3;
+    int n;
+    cin >> n;
+    cout << dp[n] << '\n';
+
+    return 0;
+}

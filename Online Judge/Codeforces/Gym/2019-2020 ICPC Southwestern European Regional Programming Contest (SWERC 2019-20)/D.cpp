92e383a0b6282677701fa4eeac184e6974f16f03
Muhammad Hasan <muhammadhasan50@gmail.com>
2021-01-28 10:49:42 +0700

    add D solution SWERC 2019

--- Online Judge/Codeforces/Gym/2019-2020 ICPC Southwestern European Regional Programming Contest (SWERC 2019-20)/D.cpp
+++ Online Judge/Codeforces/Gym/2019-2020 ICPC Southwestern European Regional Programming Contest (SWERC 2019-20)/D.cpp
@@ -29,14 +29,64 @@ sim dor(const c&) { ris; }
 };
 #define imie(...) "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
 
-
+const int N = 2e4 + 10;
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
+    string S, T;
+    cin >> S >> T;
 
+    map<pair<int, int>, int> mp;
+    vector<pair<int, int>> memo(N);
+    int id = 0;
+    
+    auto getId = [&](pair<int, int> p) -> int {
+        if (mp.count(p) == 0) {
+            memo[id] = p;
+            mp[p] = id++;
+        }
+        return mp[p];
+    };
+    
+    auto solve = [&](string& X) -> deque<int> {
+        deque<int> dq;
+        for (int i = 2; i < N; i++) {
+            dq.emplace_back(-i);
+        }
+        for (char cc : X) {
+            if (cc == 'C') {
+                dq.emplace_front(dq.front());
+            } else if (cc == 'D') {
+                dq.pop_front();
+            } else if (cc == 'L') {
+                if (dq.front() < 0) return {-1};
+                dq.front() = memo[dq.front()].first;
+            } else if (cc == 'P') {
+                int a = dq.front(); dq.pop_front();
+                int b = dq.front(); dq.pop_front();
+                dq.emplace_front(getId(make_pair(a, b)));
+            } else if (cc == 'R') {
+                if (dq.front() < 0) return {-1};
+                dq.front() = memo[dq.front()].second;
+            } else if (cc == 'S') {
+                int a = dq.front(); dq.pop_front();
+                int b = dq.front(); dq.pop_front();
+                dq.emplace_front(a); dq.emplace_front(b);
+            } else if (cc == 'U') {
+                if (dq.front() < 0) return {-1};
+                int a = dq.front();
+                dq.pop_front();
+                dq.emplace_front(memo[a].second);
+                dq.emplace_front(memo[a].first);
+            }
+        }
+        return dq;
+    };
+
+    cout << (solve(S) == solve(T) ? "True" : "False") << '\n';
 
     return 0;
 }

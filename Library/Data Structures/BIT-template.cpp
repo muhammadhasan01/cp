3aa100f5f30709f91205b72bdce809bbc27c0bc7
Muhammad Hasan <muhammadhasan50@gmail.com>
2021-02-03 11:33:27 +0700

    add BIT Template

---
+++ Library/Data Structures/BIT-template.cpp
@@ -0,0 +1,23 @@
+template<typename T>
+struct BIT {
+    int n;
+    vector<T> bit;
+    
+    BIT(int sz) : n(sz), bit(sz + 1) {}
+
+    void add(int x, T val) {
+        for ( ; x <= n; x += x & -x)
+            bit[x] += val;
+    }
+
+    T get(int x) {
+        T ret = 0;
+        for ( ; x > 0; x -= x & -x)
+            ret += bit[x];
+        return ret;
+    }
+
+    T query(int l, int r) {
+        return get(r) - get(l - 1);
+    }
+};

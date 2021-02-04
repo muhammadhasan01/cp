3575ede56f0c0e5582773673156a7068fed51fdf
Muhamamad Hasan <muhammadhasan50@gmail.com>
2021-02-04 21:46:57 +0700

    add DSU template

---
+++ Library/Data Structures/DSU.cpp
@@ -0,0 +1,21 @@
+struct DSU {
+    int n;
+    vector<int> par, sz;
+    
+    DSU(int _n) : n(_n), par(_n), sz(_n) {
+        iota(par.begin(), par.end(), 0);
+    }
+
+    int fpar(int x) {
+        return (par[x] == x ? x : fpar(par[x]));
+    }
+
+    bool merge(int u, int v) {
+        int pu = fpar(u), pv = fpar(v);
+        if (pu == pv) return false;
+        if (sz[pu] < sz[pv]) swap(pu, pv);
+        par[pv] = pu;
+        sz[pu] += sz[pv];
+        return true;
+    }
+};

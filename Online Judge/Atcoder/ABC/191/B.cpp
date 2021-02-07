7967300dab75da047ae84eb615d09db01cb2766e
Muhamamad Hasan <muhammadhasan50@gmail.com>
2021-02-08 05:45:33 +0700

    add B solution ABC191

---
+++ Online Judge/Atcoder/ABC/191/B.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+const int N = 1e5 + 5;
+
+int n, x;
+int a[N];
+
+void solve() {
+    cin >> n >> x;
+    vector<int> res;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        if (a[i] == x) continue;
+        res.emplace_back(a[i]);
+    }
+    int len = res.size();
+    for (int i = 0; i < len; i++) {
+        cout << res[i] << " \n"[i == len - 1];
+    }
+}
+
+int main() { 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    for (int T = 1; T <= tc; T++) {
+        solve();
+    }
+
+    return 0;
+}

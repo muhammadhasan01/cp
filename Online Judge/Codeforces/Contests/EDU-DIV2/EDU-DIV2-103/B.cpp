30a092cd14495c7eb15fdfe922326d372ab2efa7
Muhammad Hasan <muhammadhasan50@gmail.com>
2021-02-15 07:01:51 +0700

    Add B solution EDU-DIV2-103

---
+++ Online Judge/Codeforces/Contests/EDU-DIV2/EDU-DIV2-103/B.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+const long long INF = 1e16;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<long long> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    auto can = [&](long long x) {
+        long long sum = x + v[0];
+        for (int i = 1; i < n; i++) {
+            if (v[i] * 100 > sum * k) return false;
+            sum += v[i];
+        }
+        return true;
+    };
+
+    long long l = 0, r = INF, ans = 0;
+    while (l <= r) {
+        long long mid = (l + r) >> 1;
+        if (can(mid)) {
+            r = mid - 1;
+            ans = mid;
+        } else {
+            l = mid + 1;
+        }
+    }
+    cout << ans << '\n';
+}
+
+int main() { 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc;
+    cin >> tc;
+    for (int T = 1; T <= tc; T++) {
+        solve();
+    }
+
+    return 0;
+}

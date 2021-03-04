24121b79e81ebda53a88cb9ef7fde9d77d3c5a6a
Muhammad Hasan <muhammadhasan50@gmail.com>
2021-03-04 08:57:19 +0700

    add Mint to library

---
+++ Library/Math/Mint.cpp
@@ -0,0 +1,74 @@
+struct mint
+{
+    const int MOD = 1e9 + 7;
+
+    int x;
+
+    mint(int _x) : x(_x) {}
+
+    mint() : x(1) {}
+
+    mint &operator=(const mint &rhs)
+    {
+        x = rhs.x;
+        return *this;
+    }
+
+    mint pow(long long n) const
+    {
+        assert(0 <= n);
+        mint x = *this, r = mint(1);
+        while (n)
+        {
+            if (n & 1)
+                r *= x;
+            x *= x;
+            n >>= 1;
+        }
+        return r;
+    }
+    mint inv() const
+    {
+        return pow(MOD - 2);
+    }
+
+    mint &operator+=(const mint &rhs)
+    {
+        x += rhs.x;
+        if (x >= MOD)
+            x -= MOD;
+        return *this;
+    }
+    mint &operator-=(const mint &rhs)
+    {
+        x -= rhs.x;
+        if (x < 0)
+            x += MOD;
+        return *this;
+    }
+    mint &operator*=(const mint &rhs)
+    {
+        unsigned long long z = x;
+        z *= rhs.x;
+        x = (unsigned int)(z % MOD);
+        return *this;
+    }
+    mint &operator/=(const mint &rhs) { return *this = *this * rhs.inv(); }
+
+    friend mint operator+(const mint &lhs, const mint &rhs)
+    {
+        return mint(lhs) += rhs;
+    }
+    friend mint operator-(const mint &lhs, const mint &rhs)
+    {
+        return mint(lhs) -= rhs;
+    }
+    friend mint operator*(const mint &lhs, const mint &rhs)
+    {
+        return mint(lhs) *= rhs;
+    }
+    friend mint operator/(const mint &lhs, const mint &rhs)
+    {
+        return mint(lhs) /= rhs;
+    }
+};

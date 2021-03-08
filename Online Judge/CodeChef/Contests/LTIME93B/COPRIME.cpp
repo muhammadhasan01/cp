2bca760455ed9cc9b699ddc95e21a5abe26af95a
Muhammad Hasan <muhammadhasan50@gmail.com>
2021-03-08 14:04:58 +0700

    add COPRIME solution LTIME93B

---
+++ Online Judge/CodeChef/Contests/LTIME93B/COPRIME.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int ans = 1e9 + 7;
+
+void solve() {
+    int L, R;
+    cin >> L >> R;
+    cout << ans << '\n';
+}
+
+int main() { 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc;
+    cin >> tc;
+    for (int T = 1; T <= tc; T++) {
+        solve();
+    }
+
+    return 0;
+}

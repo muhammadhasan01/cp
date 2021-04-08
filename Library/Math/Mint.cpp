d0164aa47d06fb9b110125f8b5bb29b483e8a1ad
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-04-08 07:20:57 +0700

    style: change mint library style

--- Library/Math/Mint.cpp
+++ Library/Math/Mint.cpp
@@ -1,21 +1,17 @@
-struct mint
-{
+struct mint {
     const int MOD = 1e9 + 7;
-
     int x;
 
     mint(int _x) : x(_x) {}
 
-    mint() : x(1) {}
+    mint() : x(0) {}
 
-    mint &operator=(const mint &rhs)
-    {
+    mint &operator=(const mint &rhs) {
         x = rhs.x;
         return *this;
     }
 
-    mint pow(long long n) const
-    {
+    mint pow(long long n) const {
         assert(0 <= n);
         mint x = *this, r = mint(1);
         while (n)
@@ -27,48 +23,49 @@ struct mint
         }
         return r;
     }
-    mint inv() const
-    {
+
+    mint inv() const {
         return pow(MOD - 2);
     }
 
-    mint &operator+=(const mint &rhs)
-    {
+    mint &operator+=(const mint &rhs) {
         x += rhs.x;
         if (x >= MOD)
             x -= MOD;
         return *this;
     }
-    mint &operator-=(const mint &rhs)
-    {
+
+    mint &operator-=(const mint &rhs) {
         x -= rhs.x;
         if (x < 0)
             x += MOD;
         return *this;
     }
-    mint &operator*=(const mint &rhs)
-    {
+
+    mint &operator*=(const mint &rhs) {
         unsigned long long z = x;
         z *= rhs.x;
         x = (unsigned int)(z % MOD);
         return *this;
     }
-    mint &operator/=(const mint &rhs) { return *this = *this * rhs.inv(); }
 
-    friend mint operator+(const mint &lhs, const mint &rhs)
-    {
+    mint &operator/=(const mint &rhs) {
+        return *this = *this * rhs.inv();
+    }
+
+    friend mint operator+(const mint &lhs, const mint &rhs) {
         return mint(lhs) += rhs;
     }
-    friend mint operator-(const mint &lhs, const mint &rhs)
-    {
+
+    friend mint operator-(const mint &lhs, const mint &rhs) {
         return mint(lhs) -= rhs;
     }
-    friend mint operator*(const mint &lhs, const mint &rhs)
-    {
+
+    friend mint operator*(const mint &lhs, const mint &rhs) {
         return mint(lhs) *= rhs;
     }
-    friend mint operator/(const mint &lhs, const mint &rhs)
-    {
+
+    friend mint operator/(const mint &lhs, const mint &rhs) {
         return mint(lhs) /= rhs;
     }
 };

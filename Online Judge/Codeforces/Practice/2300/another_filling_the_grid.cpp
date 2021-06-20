468be1af323c9587eb87290139dad7885c94e0cf
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-06-20 16:16:11 +0700

    sol: add practice solution

---
+++ Online Judge/Codeforces/Practice/2300/another_filling_the_grid.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct mint {
+    const int MOD = 1e9 + 7;
+    int x;
+
+    mint(int _x) : x((_x % MOD + MOD) % MOD) {}
+
+    mint() : x(0) {}
+
+    mint &operator=(const mint &rhs) {
+        x = rhs.x;
+        return *this;
+    }
+
+    mint pow(long long n) const {
+        assert(0 <= n);
+        mint x = *this, r = mint(1);
+        while (n)
+        {
+            if (n & 1)
+                r *= x;
+            x *= x;
+            n >>= 1;
+        }
+        return r;
+    }
+
+    mint inv() const {
+        return pow(MOD - 2);
+    }
+
+    mint &operator+=(const mint &rhs) {
+        x += rhs.x;
+        if (x >= MOD)
+            x -= MOD;
+        return *this;
+    }
+
+    mint &operator-=(const mint &rhs) {
+        x -= rhs.x;
+        if (x < 0)
+            x += MOD;
+        return *this;
+    }
+
+    mint &operator*=(const mint &rhs) {
+        unsigned long long z = x;
+        z *= rhs.x;
+        x = (unsigned int)(z % MOD);
+        return *this;
+    }
+
+    mint &operator/=(const mint &rhs) {
+        return *this = *this * rhs.inv();
+    }
+
+    friend mint operator+(const mint &lhs, const mint &rhs) {
+        return mint(lhs) += rhs;
+    }
+
+    friend mint operator-(const mint &lhs, const mint &rhs) {
+        return mint(lhs) -= rhs;
+    }
+
+    friend mint operator*(const mint &lhs, const mint &rhs) {
+        return mint(lhs) *= rhs;
+    }
+
+    friend mint operator/(const mint &lhs, const mint &rhs) {
+        return mint(lhs) /= rhs;
+    }
+};
+
+const int N = 255;
+
+int n, k;
+mint C[N][N];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    C[0][0] = 1;
+    for (int i = 1; i < N; i++) {
+        C[i][0] = 1;
+        for (int j = 1; j <= i; j++) {
+            C[i][j] = C[i - 1][j] + C[i - 1][j - 1];
+        }
+    }
+
+    cin >> n >> k;
+    if (k == 1) {
+        cout << 1 << '\n';
+        return 0;
+    }
+    mint ans = 0;
+    for (int i = 0; i <= n; i++) {
+        mint sign = (i % 2 == 0 ? 1 : -1);
+        ans += sign * C[n][i] * ((mint(k).pow(n - i) * mint(k - 1).pow(i) - mint(k - 1).pow(n)).pow(n));
+    }
+    cout << ans.x << '\n';
+
+    return 0;
+}

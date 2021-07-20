0b15c1b6e1f6ccc0a8777d46a6fcf5b5396a3ead
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-07-21 00:23:38 +0700

    sol: add solution A TROC 20

---
+++ Online Judge/TLX/TROC#20/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int x, y;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> x >> y;
+    cout << (x >= y ? "YES" : "NO") << '\n';
+
+    return 0;
+}

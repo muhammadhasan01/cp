7c145cb196875db53a6354492f30724aad7340f8
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-08-02 20:20:26 +0700

    lib: update space for sp

--- Library/Data Structures/Sparse_Table.cpp
+++ Library/Data Structures/Sparse_Table.cpp
@@ -2,36 +2,36 @@
 // https://cp-algorithms.com/data_structures/sparse-table.html
 template<int MAXN, int MAXLOG, typename T>
 struct sparse_table {
-	int n;
-	vector<vector<T>> dp;
-	vector<int> prec_lg2;
+    int n;
+    vector<vector<T>> dp;
+    vector<int> prec_lg2;
 
-	sparse_table() {
-		dp.assign(MAXN, vector<T>(MAXLOG, 0));
-		prec_lg2.assign(MAXN, 0);
-	}
+    sparse_table() {
+        dp.assign(MAXN, vector<T>(MAXLOG, 0));
+        prec_lg2.assign(MAXN, 0);
+    }
 
-	T combine(const T& a, const T& b) {
-		return min(a, b);
-	}
+    T combine(const T& a, const T& b) {
+        return min(a, b);
+    }
 
-	void init(vector<T> &a) {
-		n = a.size();
-		for (int i = 2; i < 2 * n; i++) {
-			prec_lg2[i] = prec_lg2[i >> 1] + 1;
-		}
-		for (int i = 0; i < n; i++) {
-			dp[i][0] = a[i];
-		}
-		for (int j = 1; (1 << j) <= n; j++) {
-			for (int i = 0; i < n; i++) {
-				dp[i][j] = combine(dp[i][j - 1], dp[i + (1 << (j - 1))][j - 1]);
-			}
-		}
-	}
+    void init(vector<T> &a) {
+        n = a.size();
+        for (int i = 2; i < 2 * n; i++) {
+            prec_lg2[i] = prec_lg2[i >> 1] + 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = a[i];
+        }
+        for (int j = 1; (1 << j) <= n; j++) {
+            for (int i = 0; i < n; i++) {
+                dp[i][j] = combine(dp[i][j - 1], dp[i + (1 << (j - 1))][j - 1]);
+            }
+        }
+    }
 
-	T query(int l, int r) {
-		int k = prec_lg2[r - l + 1];
-		return combine(dp[l][k], dp[r - (1 << k) + 1][k]);
-	}
+    T query(int l, int r) {
+        int k = prec_lg2[r - l + 1];
+        return combine(dp[l][k], dp[r - (1 << k) + 1][k]);
+    }
 };

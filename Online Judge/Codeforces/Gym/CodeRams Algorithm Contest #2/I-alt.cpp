f765f0852692eb2a539f47ca4a09d13e5c108689
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-08-03 21:14:52 +0700

    chore: change I alternative

--- Online Judge/Codeforces/Gym/CodeRams Algorithm Contest #2/I-alt.cpp
+++ Online Judge/Codeforces/Gym/CodeRams Algorithm Contest #2/I-alt.cpp
@@ -1,112 +1,72 @@
-/*   AUTHOR: julianferres   */
 #include <bits/stdc++.h>
+
 using namespace std;
 
-// neal Debugger
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
- 
-void dbg_out() { cerr << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
- 
-#ifdef LOCAL
-#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
+const int N = 1e5 + 5;
 
-typedef long long ll;
-typedef vector<ll> vi; typedef pair<ll,ll> ii;
-typedef vector<ii> vii; typedef vector<bool> vb;
-#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
-#define forn(i, n) forr(i, 0, n)
-#define RAYA cerr << "===============================" << endl
-#define pb push_back
-#define mp make_pair
-#define all(c) (c).begin(),(c).end()
-#define esta(x,c) ((c).find(x) != (c).end())
-const int INF = 1e9+15; // const ll INF = 2e18;
-const int MOD = 1e9+7;  // const int MOD = 998244353;
-const int MAXN  = 1e5+5;
+int n, m, s, t;
+vector<int> adj[N];
+int tin[N], low[N], timer = 0;
+bool visited[N];
+bool is_cutpoint[N];
+bool ans[N];
+int par[N];
 
-struct arista {
-  int from, next, ind;
-};
-
-vector < vector <arista> > g(MAXN);  
-vector < arista > todos;
-vector <bool> cutpoint;
-
-vector <bool> visto;
-vector <int> tin, low;
-int timer;
-vi parent;
-
-void IS_CUTPOINT(int x) { cutpoint[x] = true; }
-
-void DFS(int x, int p = -1) {
-  visto[x] = true;
-  tin[x] = low[x] = timer++;
-  int children=0;
-  for(arista to : g[x]) {
-    if(to.next == p) continue;
-    if(visto[to.next]) low[x] = min(low[x], tin[to.next]);
-    else {
-      DFS(to.next, x);
-      parent[to.next] = x;
-      low[x] = min(low[x], low[to.next]);
-      if(low[to.next] >= tin[x] && p!=-1) IS_CUTPOINT(x);
-      ++children;
+void dfs(int u, int p = -1) {
+    visited[u] = true;
+    tin[u] = low[u] = timer++;
+    int children = 0;
+    for (int v : adj[u]) {
+        if (v == p) {
+            continue;
+        }
+        if (visited[v]) {
+            low[u] = min(low[u], tin[v]);
+        } else {
+            par[v] = u;
+            dfs(v, u);
+            low[u] = min(low[u], low[v]);
+            if (tin[u] <= low[v] && p != -1) {
+                is_cutpoint[u] = true;;
+            }
+            ++children;
+        }
     }
-  }
-  if(p == -1 && children > 1) IS_CUTPOINT(x);
+    if (p == -1 && children > 1) {
+        is_cutpoint[u] = true;
+    }
 }
 
-void find_cutpoints(int n, int a) {
-  timer = 0;
-  visto.assign(n, false);
-  tin.assign(n, -1); low.assign(n, -1);
-  parent[a] = -1;
-  DFS(a);
-}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-void add_edge(int x, int y, int i) {
-	g[x].pb({x,y,(int)i}); g[y].pb({y,x,(int)i});
-	todos.pb({x,y,(int)i});
-}
-
-
-int main(){  
-    FIN;
-
-    int n, m, a, b; cin >> n >> m >> a >> b;
-    a--, b--;
-    cutpoint.resize(n), parent.resize(n);
-
-    int edge_idx = 0;
-    forn(i, m){
-    	int k; cin >> k;
-    	vi edge(k); forn(i, k) cin >> edge[i];
-    	forn(j, k-1){
-    		add_edge(edge[j]-1, edge[j+1]-1, edge_idx++);
-    	}
+    cin >> n >> m >> s >> t;
+    for (int i = 0; i < m; i++) {
+        int len;
+        cin >> len;
+        vector<int> v(len);
+        for (int j = 0; j < len; j++) {
+            cin >> v[j];
+            if (j > 0) {
+                adj[v[j - 1]].emplace_back(v[j]);
+                adj[v[j]].emplace_back(v[j - 1]);
+            }
+        }
     }
-
-    vb ans(n, 1);
-    ans[a] = 0, ans[b] = 0;
-    find_cutpoints(n, a);
-
-    int min_low = low[b];
-
-    while(b != a){
-    	if(cutpoint[b] && min_low >= tin[b]) ans[b] = 0;
-    	min_low = min(min_low, low[b]);
-    	b = parent[b];
+    dfs(s);
+    ans[s] = ans[t] = true;
+    for (int u = t, min_low = low[u]; u != s; u = par[u]) {
+        if (is_cutpoint[u] && tin[u] <= min_low) {
+            ans[u] = true;
+        }
+        min_low = min(min_low, low[u]);
     }
-    forn(i, n) cout << ans[i];
-    cout << "\n";
-
+    for (int i = 1; i <= n; i++) {
+        cout << !ans[i];
+    }
+    cout << '\n';
 
     return 0;
 }

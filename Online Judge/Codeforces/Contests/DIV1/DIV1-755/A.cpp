0d7468798e381fb2c6f1761aeed0cce2e8c714d4
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-11-17 08:55:49 +0700

    sol: add solution DIV1-755A

---
+++ Online Judge/Codeforces/Contests/DIV1/DIV1-755/A.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    for (int i = 0; i < n; i++) {
+        if (a[i] == b[i] || a[i] + 1 == b[i]) {
+            continue;
+        }
+        cout << "NO" << '\n';
+        return;
+    }
+    cout << "YES" << '\n';
+}
+
+int main() { 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        solve();
+    }
+    
+    return 0;
+}

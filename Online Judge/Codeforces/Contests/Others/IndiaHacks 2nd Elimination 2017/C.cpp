f789b6f0f488350ebb31795fcb0bcaf8899de446
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-11-21 12:36:26 +0700

    sol: add solution C IH2017

---
+++ Online Judge/Codeforces/Contests/Others/IndiaHacks 2nd Elimination 2017/C.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct mint {
+    const int MOD = 1e9 + 7;
+    int x;
+
+    mint(int _x) : x((_x % MOD + MOD) % MOD) {}
+    mint(long long _x) : x((_x % MOD + MOD) % MOD) {}
+
+    mint() : x(0) {}
+
+    mint &operator=(const mint &rhs) {
+        x = rhs.x;
+        return *this;
+    }
+
+    mint pow(long long n) const {
+        assert(0 <= n);
+        mint x = *this, r = mint(1);
+        while (n)
+        {
+            if (n & 1)
+                r *= x;
+            x *= x;
+            n >>= 1;
+        }
+        return r;
+    }
+
+    mint inv() const {
+        return pow(MOD - 2);
+    }
+
+    mint &operator+=(const mint &rhs) {
+        x += rhs.x;
+        if (x >= MOD)
+            x -= MOD;
+        return *this;
+    }
+
+    mint &operator-=(const mint &rhs) {
+        x -= rhs.x;
+        if (x < 0)
+            x += MOD;
+        return *this;
+    }
+
+    mint &operator*=(const mint &rhs) {
+        unsigned long long z = x;
+        z *= rhs.x;
+        x = (unsigned int)(z % MOD);
+        return *this;
+    }
+
+    mint &operator/=(const mint &rhs) {
+        return *this = *this * rhs.inv();
+    }
+
+    friend mint operator+(const mint &lhs, const mint &rhs) {
+        return mint(lhs) += rhs;
+    }
+
+    friend mint operator-(const mint &lhs, const mint &rhs) {
+        return mint(lhs) -= rhs;
+    }
+
+    friend mint operator*(const mint &lhs, const mint &rhs) {
+        return mint(lhs) *= rhs;
+    }
+
+    friend mint operator/(const mint &lhs, const mint &rhs) {
+        return mint(lhs) /= rhs;
+    }
+
+    friend ostream& operator<<(ostream &os, const mint &m) {
+        return os << m.x;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int n, m;
+    cin >> n >> m;
+    mint ans = mint(2 * (n + 1)).pow(m) * mint(n + 1 - m) / mint(n + 1);
+    cout << ans << '\n';
+    
+    return 0;
+}

4780ba4949014562b84740759d220751d40f80c4
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-12-04 20:27:05 +0700

    sol: add solution D EDU-DIV2-118

---
+++ Online Judge/Codeforces/Contests/EDU-DIV2/EDU-DIV2-118/D.cpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+struct mint {
+    const int MOD = 998244353;
+    int x;
+
+    mint(int _x) : x((_x % MOD + MOD) % MOD) {}
+    mint(long long _x) : x((_x % MOD + MOD) % MOD) {}
+
+    mint() : x(0) {}
+
+    mint &operator=(const mint &rhs) {
+        x = rhs.x;
+        return *this;
+    }
+
+    mint pow(long long n) const {
+        assert(0 <= n);
+        mint x = *this, r = mint(1);
+        while (n)
+        {
+            if (n & 1)
+                r *= x;
+            x *= x;
+            n >>= 1;
+        }
+        return r;
+    }
+
+    mint inv() const {
+        return pow(MOD - 2);
+    }
+
+    mint &operator+=(const mint &rhs) {
+        x += rhs.x;
+        if (x >= MOD)
+            x -= MOD;
+        return *this;
+    }
+
+    mint &operator-=(const mint &rhs) {
+        x -= rhs.x;
+        if (x < 0)
+            x += MOD;
+        return *this;
+    }
+
+    mint &operator*=(const mint &rhs) {
+        unsigned long long z = x;
+        z *= rhs.x;
+        x = (unsigned int)(z % MOD);
+        return *this;
+    }
+
+    mint &operator/=(const mint &rhs) {
+        return *this = *this * rhs.inv();
+    }
+
+    friend mint operator+(const mint &lhs, const mint &rhs) {
+        return mint(lhs) += rhs;
+    }
+
+    friend mint operator-(const mint &lhs, const mint &rhs) {
+        return mint(lhs) -= rhs;
+    }
+
+    friend mint operator*(const mint &lhs, const mint &rhs) {
+        return mint(lhs) *= rhs;
+    }
+
+    friend mint operator/(const mint &lhs, const mint &rhs) {
+        return mint(lhs) /= rhs;
+    }
+
+    friend ostream& operator<<(ostream &os, const mint &m) {
+        return os << m.x;
+    }
+};
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    mint ans = 0;
+    vector<vector<mint>> dp(n + 5, vector<mint>(2));
+    dp[0][0] = 1;
+    for (int x : a) {
+        // X: ndp[x - 1][1] += dp[x - 1][0] + dp[x - 1][1]
+        // Y: ndp[x + 1][0] += dp[x][0] + dp[x + 1][0]
+        // Z: ndp[x + 1][1] += dp[x + 1][1]
+        mint X = (x - 1 >= 0 ? dp[x - 1][0] + dp[x - 1][1] : 0);
+        mint Y = dp[x][0] + dp[x + 1][0];
+        mint Z = dp[x + 1][1];
+        ans += (X + Y + Z);
+        if (x - 1 >= 0) {
+            dp[x - 1][1] += X;
+        }
+        dp[x + 1][0] += Y;
+        dp[x + 1][1] += Z;
+        debug() << imie(x) imie(X) imie(Y);
+        debug() << imie(ans);
+        debug() << imie(dp[0]) imie(dp[1]);
+    }
+    cout << ans << '\n';
+}
+
+int main() { 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        solve();
+    }
+    
+    return 0;
+}

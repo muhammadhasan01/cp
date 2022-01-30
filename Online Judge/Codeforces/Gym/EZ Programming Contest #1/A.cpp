3960d6130e0055cad7f3594d67609d9aa2fd08de
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2022-01-30 10:27:55 +0700

    sol: add solution A EZPC#1

---
+++ Online Judge/Codeforces/Gym/EZ Programming Contest #1/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    cout << v[n - 1] - v[0] << '\n';
+}
+
+int main() { 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc;
+    cin >> tc;
+    for (int T = 1; T <= tc; T++) {
+        solve();
+    }
+
+    return 0;
+}

9283aa5b69b8a1ac0373e39a708cb06a003f58d1
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-04-01 09:36:43 +0700

    sol: add ice cream solution

---
+++ Lomba Lomba/2022/Shopee Code League/final/ice-cream-bf.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct mint {
+    const int MOD = 998244353;
+    int x;
+
+    mint(int _x) : x((_x % MOD + MOD) % MOD) {}
+    mint(long long _x) : x((_x % MOD + MOD) % MOD) {}
+
+    mint() : x(0) {}
+
+    mint &operator=(const mint &rhs) {
+        x = rhs.x;
+        return *this;
+    }
+
+    mint pow(long long n) const {
+        assert(0 <= n);
+        mint x = *this, r = mint(1);
+        while (n)
+        {
+            if (n & 1)
+                r *= x;
+            x *= x;
+            n >>= 1;
+        }
+        return r;
+    }
+
+    mint inv() const {
+        return pow(MOD - 2);
+    }
+
+    mint &operator+=(const mint &rhs) {
+        x += rhs.x;
+        if (x >= MOD)
+            x -= MOD;
+        return *this;
+    }
+
+    mint &operator-=(const mint &rhs) {
+        x -= rhs.x;
+        if (x < 0)
+            x += MOD;
+        return *this;
+    }
+
+    mint &operator*=(const mint &rhs) {
+        unsigned long long z = x;
+        z *= rhs.x;
+        x = (unsigned int)(z % MOD);
+        return *this;
+    }
+
+    mint &operator/=(const mint &rhs) {
+        return *this = *this * rhs.inv();
+    }
+
+    friend mint operator+(const mint &lhs, const mint &rhs) {
+        return mint(lhs) += rhs;
+    }
+
+    friend mint operator-(const mint &lhs, const mint &rhs) {
+        return mint(lhs) -= rhs;
+    }
+
+    friend mint operator*(const mint &lhs, const mint &rhs) {
+        return mint(lhs) *= rhs;
+    }
+
+    friend mint operator/(const mint &lhs, const mint &rhs) {
+        return mint(lhs) /= rhs;
+    }
+
+    friend ostream& operator<<(ostream &os, const mint &m) {
+        return os << m.x;
+    }
+};
+
+const int N = 1e6 + 5;
+ 
+mint fact[N];
+mint invf[N];
+ 
+mint C(int a, int b) {
+    if (a < b) return 0;
+    return fact[a] * invf[b] * invf[a - b];
+}
+ 
+void init() {
+    fact[0] = invf[0] = 1;
+    for (int i = 1; i < N; i++) {
+        fact[i] = fact[i - 1] * i;
+        invf[i] = fact[i].inv();
+    }
+}
+
+// T(n, k):= sum(binomial(k, i)*binomial(n+2*k-i-1, 3*k-1), i, 0, n-k)
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    init();
+    int n, m;
+    cin >> n >> m;
+    mint ans = 0;
+    for (int i = 0; i <= n - m; i++) {
+        ans += C(m, i) * C(n + 2 * m - i - 1, 3 * m - 1);
+    }
+    cout << ans << '\n';
+
+    return 0;
+}

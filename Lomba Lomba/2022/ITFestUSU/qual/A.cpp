3d4437fc6981a35a416a5893ec3261c90a826b19
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-04-12 10:18:40 +0700

    sol: add solution A qual ITFest USU

---
+++ Lomba Lomba/2022/ITFestUSU/qual/A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (abs(a[i] - a[j]) <= x) {
+                ++ans;
+            }
+        }
+    }
+    cout << ans << '\n';
+
+    return 0;
+}
